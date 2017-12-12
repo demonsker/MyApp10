def0c05fed4eaf38491430e3148ad17539dfbb1d
Patipon Taweechat <57050269@kmitl.ac.th>
2017-12-12 13:41:40 +0700

    Add Session

---
+++ app/src/main/java/com/example/happypig/myapplication/Utilities/Session.java
@@ -0,0 +1,29 @@
+package com.example.happypig.myapplication.Utilities;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+/**
+ * Created by Yhingkai on 12/12/2017.
+ */
+
+public class Session {
+
+    private SharedPreferences prefs;
+
+    public Session(Context cntx) {
+        // TODO Auto-generated constructor stub
+        prefs = PreferenceManager.getDefaultSharedPreferences(cntx);
+    }
+
+    public void setUserName(String userName) {
+        prefs.edit().putString("useName", userName).commit();
+    }
+
+    public String getUserName() {
+        String userName = prefs.getString("userName","");
+        return userName;
+    }
+
+}
