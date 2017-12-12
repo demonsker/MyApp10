b6ec21555f2cac2c3e14304235617e5abf994b45
Patipon Taweechat <57050269@kmitl.ac.th>
2017-12-12 19:40:23 +0700

    Add Override BackPress to PigstyActivity

--- app/src/main/java/com/example/happypig/myapplication/PigstyActivity.java
+++ app/src/main/java/com/example/happypig/myapplication/PigstyActivity.java
@@ -3,8 +3,11 @@ package com.example.happypig.myapplication;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.widget.Toast;
 
 public class PigstyActivity extends AppCompatActivity {
+
+    private int exit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +16,16 @@ public class PigstyActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        exit = 1;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if( exit-- == 0) {
+            finish();
+            System.exit(0);
+        }
+        else
+            Toast.makeText(getApplicationContext(),"กดอีกครั้งเพื่อออกจากแอพพลิเคชัน",Toast.LENGTH_LONG).show();
     }
 }
