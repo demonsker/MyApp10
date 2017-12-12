94837857751a264e00cab96c8d71b13eadad54b6
Patipon Taweechat <57050269@kmitl.ac.th>
2017-12-12 15:02:15 +0700

    Add Sesssion to PINActivity

--- app/src/main/java/com/example/happypig/myapplication/PINActivity.java
+++ app/src/main/java/com/example/happypig/myapplication/PINActivity.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Toast;
+
+import com.example.happypig.myapplication.Utilities.Session;
 
 public class PINActivity extends AppCompatActivity {
 
@@ -20,8 +23,16 @@ public class PINActivity extends AppCompatActivity {
 
     public  void  enterPinClick (View v)
     {
-        Intent intentdoor = new Intent(getBaseContext(), PigstyActivity.class);
-        startActivity(intentdoor);
+        Session session = new Session(getApplicationContext());
+        String pin = "";
+
+        if(pin.equals(session.getPin())) {
+            Intent intentdoor = new Intent(getBaseContext(), PigstyActivity.class);
+            startActivity(intentdoor);
+        }
+        else{
+            Toast.makeText(getApplicationContext(),"PIN ไม่ถูกต้อง",Toast.LENGTH_LONG).show();
+        }
     }
 
     public  void  skipClick (View v)
