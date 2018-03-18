983b2b6b3d77d5893c3e0ed56ab9b92c2a102a70
ITIS <57050194@kmitl.ac.th>
2018-03-18 21:33:51 +0700

    Add Search pig box and controller

--- app/src/main/java/com/example/happypig/myapplication/SubMainActivity.java
+++ app/src/main/java/com/example/happypig/myapplication/SubMainActivity.java
@@ -7,11 +7,22 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import com.example.happypig.myapplication.models.Pig;
 
+import java.util.ArrayList;
+
 public class SubMainActivity extends AppCompatActivity {
+
+    static Button button;
+    static EditText pigSearch;
+    static ListView listview;
+    static AppCompatActivity appContext;
+
+    static String[] pigArray;
+    static ArrayAdapter<String> arrayAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,32 +33,54 @@ public class SubMainActivity extends AppCompatActivity {
 
     }
 
+    private static void mapLayout(AppCompatActivity app){
+        appContext = app;
+        button = (Button) app.findViewById(R.id.testButton);
+        pigSearch = (EditText) app.findViewById(R.id.pigsearch);
+        listview = (ListView) app.findViewById(R.id.listview1);
+    }
+
+    private static void convertToArray(ArrayList<Pig> list, String[] array){
+        for(int i = 0; i < list.size(); i++)
+            array[i] = list.get(i).getName();
+    }
+
     static void addController(AppCompatActivity app){
-        Button button = (Button) app.findViewById(R.id.testButton);
-        button.setOnClickListener(new Button.OnClickListener() {
-            public void onClick(View v) {
-                System.out.println("Controller work!!");
-            }
-        });
+        mapLayout(app);
 
         Pig pig1 = new Pig("0001", "John");
         Pig pig2 = new Pig("0002", "Alex");
         Pig pig3 = new Pig("0003", "Wine");
+        ArrayList<Pig> pigList = new ArrayList<>();
+        pigList.add(pig1);
+        pigList.add(pig2);
+        pigList.add(pig3);
 
-        final String[] pigList = new String[3];
-        pigList[0] = pig1.getName();
-        pigList[1] = pig2.getName();
-        pigList[2] = pig3.getName();
+        pigArray = new String[pigList.size()];;
+        convertToArray(pigList, pigArray);
 
-        ListView listview = (ListView) app.findViewById(R.id.listview1);
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(app, R.layout.listview_content, R.id.textView, pigList);
+        arrayAdapter = new ArrayAdapter<String>(appContext, R.layout.listview_content, R.id.textView, pigArray);
         listview.setAdapter(arrayAdapter);
+
+        button.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+                String pigName = pigSearch.getText().toString();
+                //TODO : Search pigName in Database
+                pigArray = new String[1];
+                pigArray[0] = pigName;
+                arrayAdapter = new ArrayAdapter<String>(appContext, R.layout.listview_content, R.id.textView, pigArray);
+                listview.setAdapter(arrayAdapter);
+            }
+        });
+
         listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> arg0, View arg1, int position, long arg3) {
-                System.out.println(pigList[position]);
+                System.out.println(pigArray[position]);
             }
         });
+
+
     }
 
 }
